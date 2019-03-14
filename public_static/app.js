18e2514cdd1d1b271e7f91b864ed2f1343204ca8
gaurav1209 <aggarwal.gaurav10@gmail.com>
2019-03-14 19:10:58 +0530

    Styling and functionality added

--- public_static/app.js
+++ public_static/app.js
@@ -1,6 +1,6 @@
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
-var link = document.createElement('a');
+// var link = document.createElement('a');
 var gumStream; 						//stream from getUserMedia()
 var rec; 							//Recorder.js object
 var input; 							//MediaStreamAudioSourceNode we'll be recording
@@ -12,11 +12,15 @@ var audioContext; //audio context to help us record
 var recordButton = document.getElementById("recordButton");
 var stopButton = document.getElementById("stopButton");
 var pauseButton = document.getElementById("pauseButton");
-var sendBtn = document.getElementById("sendBtn");
+var sendButton = document.getElementById("sendBtn");
+var saveSound = document.getElementById('saveSound');
+var ol = document.getElementById('ol');
+
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 pauseButton.addEventListener("click", pauseRecording);
+sendButton.addEventListener("click", sendRecording);
 
 function startRecording() {
 	console.log("recordButton clicked");    
@@ -30,20 +34,10 @@ function startRecording() {
 		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 	
 		audioContext = new AudioContext();
-
-		//update the format 
-		// document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
-
-		/*  assign to gumStream for later use  */
 		gumStream = stream;
 		
 		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
-
-		/* 
-			Create the Recorder object and configure to record mono sound (1 channel)
-			Recording 2 channels  will double the file size
-		*/
 		rec = new Recorder(input,{numChannels:1})
 
 		//start the recording process
@@ -52,11 +46,9 @@ function startRecording() {
 		console.log("Recording started");
 
 	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
     	recordButton.disabled = false;
     	stopButton.disabled = true;
 		pauseButton.disabled = true;
-		sendBtn.disabled=true;
 	});
 }
 
@@ -76,106 +68,52 @@ function pauseRecording(){
 
 function stopRecording() {
 	console.log("stopButton clicked");
-
-	//disable the stop button, enable the record too allow for new recordings
 	stopButton.disabled = true;
 	recordButton.disabled = false;
 	pauseButton.disabled = true;
-	sendBtn.disabled=true;
-	//reset button just in case the recording is stopped while paused
 	pauseButton.innerHTML="Pause";
-	
-	//tell the recorder to stop the recording
 	rec.stop();
-
-	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
-
-	//create the wav blob and pass it on to createDownloadLink
-	rec.exportWAV(createDownloadLink);
 }
 
-function createDownloadLink(blob) {
+function sendRecording(){
+	gumStream.getAudioTracks()[0].stop();
+	rec.exportWAV(saveToServer);
+}
+
+function saveToServer(blob) {
 	
 	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var file = new File([blob], "sound.wav")
 
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
+	createLink(url);
 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
-
-	//save to disk link
-	link.href = url;
-	link.download = filename+".mp3"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
-
-	//add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".mp3 "))
-
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	//upload link
-	var upload = document.createElement('a');
-	upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		
-		//TEST
+	var file = new File([blob], "sound.wav");
+	saveSound.href = url;
+	saveSound.download = 'sound.wav'; 
 		var formData = new FormData()
       formData.append("audio", file);
 
       console.log('File : '+ file);
       console.log('Form Data : '+ formData);
 
-
-	// 	  var xhr=new XMLHttpRequest();
-	// 	  xhr.onload=function(e) {
-	// 	      if(this.readyState === 4) {
-	// 	          console.log("Server returned: ",e.target.responseText);
-	// 	      }
-	// 	  };
-	// 	  var fd=new FormData();
-	// 	  fd.append("audio_data",blob, filename);
-	// 	  xhr.open("POST","upload.php",true);
-	// 	  xhr.send(fd);
-	// 	  console.log("audio_data",blob, filename);
-	$.ajax({
-        url: "http://localhost:5000/pitch_track",
-        method: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (data) {
-          console.log(data)
-        }
-	  })
-	})
-
-	  
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
-
-	//add the li element to the ol
-	recordingsList.appendChild(li);
-	
-	
-
-	
-
-	
+		$.ajax({
+			url: "/downloadWAV",
+			method: "POST",
+			data: formData,
+			processData: false,
+			contentType: false,
+			success: function () {
+				console.log('File Downloaded');
+			}
+		  })	
 }
-const socket = io()
-sendBtn.onclick = function () {
-	socket.emit('SEND', {
-	  msg: "shubham",
-})
+
+function createLink(url){
+	
+	var li = document.createElement('li');
+	var au = document.createElement('audio');
+
+	au.controls = true;
+	au.src = url;
+	li.appendChild(au);
+	ol.appendChild(li);
 }
--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const path=require('path')
-const parser = require('busboy-body-parser')
-const app = express(); // creates express app
-const server = http.Server(app) //creates http server using the app
-const io = socketio(server) //adds a socket on above server
-const saveFile = require('save-file')
-app.use('/',express.static(path.join(__dirname , 'public_static')))
-app.use(parser())
+const path=require('path');
+const parser = require('busboy-body-parser');
+const saveFile = require('save-file');
 
-io.on('connection', (socket) => {
-    console.log('socket connected ' + socket.id)
-  socket.on('SEND', (data) => {
-    console.log(data)
-  })
+// Express App
+const app = express(); 
+ 
+app.use('/', express.static(path.join(__dirname , 'public_static')));
+
+app.use(parser());
+
+app.post('/downloadWAV', async (req,res,next) => {
+  await saveFile(req.files.audio.data, req.files.audio.name);
+  res.send(JSON.stringify('parsing data'));
 })
 
-app.post('/pitch_track', async (req,res,next) => {
-  await saveFile(req.files.audio.data, req.files.audio.name)
-  res.send(JSON.stringify('parsing data'))
-})
-server.listen(5000,()=>console.log('Server started on http://localhost:5000'))
+app.listen(5000,() => console.log('Server started on http://localhost:5000'));
